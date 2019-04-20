b0e334e6efbba6760568299830f7d109f9cfd3d9
tauanr <31476171+tauanr@users.noreply.github.com>
2019-04-20 16:15:48 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/tutorial-image.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/tutorial-image.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+
+var myHeading = document.querySelector('h1');
+
+function setUserName(){
+    var myName = prompt('please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozila is cool, ' + myName;
+}
+
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function(){
+      setUserName();
+  };
